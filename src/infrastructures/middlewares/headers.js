b5530814b4c73c24604a9094f7ac22ed6cd499d9
suryavi <suryaplaystore325@gmail.com>
2023-12-31 17:10:39 +0530

    src: header set

--- src/infrastructures/middlewares/headers.js
+++ src/infrastructures/middlewares/headers.js
@@ -1,6 +1,6 @@
 const headers = (req, res, next) => {
-  const origin = '*';
-  res.setHeader('Access-Control-Allow-Origin', origin);
+  const origin = 'http://localhost:4000';
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4000');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', '*');
   next();
